1bdad7593e1d699a7c28bdad43e32049b5b2b50b
李泽宇 <lizeyu@lizeyudeMacBook-Pro.local>
2018-02-19 13:45:47 -0800

    improve login&signup page, change the nav-bar

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -10,7 +10,7 @@ router.get("/", function(req,res){
 		if(err)
 			console.log(err);
 		else{
-			res.render("campgrounds", {campgrounds: campgrounds, currentUser: req.user});
+			res.render("campgrounds", {campgrounds: campgrounds, currentUser: req.user, page: 'campgrounds'});
 		}
 	});
 });
